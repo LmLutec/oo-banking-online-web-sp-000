02eccef5c118994f36ac92ada35c40c95a29de1e
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-15 23:36:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
   end 
   
   def reverse_transfer
-    if @status == "complete"
+    if @status == "complete" && @status != "pending" || @status != "rejected"
       sender.balance += @amount
       receiver.balance -= @amount 
     end 
