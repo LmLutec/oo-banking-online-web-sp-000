2d206216207011a2920128250d0847e5b9ec8bb3
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-15 23:31:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Transfer
   attr_reader :sender, :receiver, :status, :amount
   
-  def initialize(sender,receiver, status)
+  def initialize(sender,receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
