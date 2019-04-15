eeee069586e99b6586ff6db60d96a935678f4479
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-15 23:17:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
     if sender.valid? 
       sender.balance -= @amount
       receiver.balance += @amount 
+      @status = "complete"
     else
       "Transaction rejected. Please check your account balance."
     end 
