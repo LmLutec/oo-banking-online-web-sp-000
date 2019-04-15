4e2627d291bcbe57496a1f96b76f2e4e1b7d3ec4
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-15 23:24:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if sender.valid? && sender.balance >= @amount 
+    if sender.valid? && sender.balance >= @amount && @status == "pending"
       sender.balance -= @amount
       receiver.balance += @amount 
       @status = "complete"
